6c3e0c0350922491d97379b318a8c6c9c5a86889
mariano89 <marianoe1989@gmail.com>
2015-04-08 22:51:57 -0700

    root

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+	include ActiveModel::SecurePassword
+
+	has_secure_password
+
+	validates :username, presence: true
+	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+	validates :password, confirmation: true
+
+end
