7d382028a7a6c08d82c039c3b7e3b75e1f9819c9
mariano89 <marianoe1989@gmail.com>
2015-04-09 23:43:46 -0700

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,14 @@ class UsersController < ApplicationController
 
 	def create
 		@user = User.new(params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation))
+		
 		if @user.valid?
 			if @user.save
-				# flash[:info]
+				flash[:info] = "Signed up successfully"
 				redirect_to users_path
 			end
 		else
-			# flash.now
+			flash.now[:danger] = @user.errors.full_messages.to_sentence
 			render :new
 		end
 	end
--- db/schema.rb
+++ db/schema.rb
@@ -21,9 +21,9 @@ ActiveRecord::Schema.define(version: 20150409050157) do
     t.string   "last_name"
     t.string   "username"
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
