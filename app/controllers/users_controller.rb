a3d6cb498a7a75537dd95522989107d3cf55d23c
mariano89 <marianoe1989@gmail.com>
2015-04-11 21:31:03 -0700

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,9 @@ class UsersController < ApplicationController
 		@user = User.new(params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation))
 		
 		if @user.valid? 
-			if @user.save
+				@user.save
 				flash[:info] = "Signed up successfully"
 				redirect_to users_path
-			end
 		else
 			flash.now[:danger] = @user.errors.full_messages.to_sentence
 			render :new
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
 
   root to: "users#new" 
 
+  resources :users
+
   get '/signup' => 'users#new'
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
-
-  resources :users
 
 end
